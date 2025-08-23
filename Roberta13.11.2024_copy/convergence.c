c58d4309a38e6645eeca9489ac15cc4c9755a9f1
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2025-08-23 11:57:41 -0300

    Delete Roberta13.11.2024_copy directory

--- Roberta13.11.2024_copy/convergence.c
+++
@@ -1,8 +0,0 @@
-#include "comum.h"
-//---check convergence---
-
-void convergence(int itc, double error, double residual_p, double residual_u, double residual_v){
-    itc++;
-    error = max(residual_p, residual_u);
-    error = max(residual_v, error);
-}   
