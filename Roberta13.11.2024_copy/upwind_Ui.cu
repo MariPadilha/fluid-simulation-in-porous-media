0f93e9a89380d8590ee00259efcbe6d1c1c59ed4
MariPadilha <lunaforever53@gmail.com>
2025-09-05 19:23:40 -0300

    mexendo em equations

--- Roberta13.11.2024_copy/upwind_Ui.cu
+++ Roberta13.11.2024_copy/upwind_Ui.cu
@@ -1,13 +1,16 @@
 #include "comum.h"
 #define idx i*(jmax+1)+j
 
-__global__ void calc_upwind_ui(double *dev_fn, double *dev_fs, double *dev_fe, double *dev_fw, double **vm, double **um, double *dev_areau_n, 
+__global__ void calc_upwind_Ui(double *dev_fn, double *dev_fs, double *dev_fe, double *dev_fw, double **vm, double **um, double *dev_areau_n, 
     double *dev_areau_s, double *dev_areau_e, double *dev_areau_w, double *dev_epsilon1, double *dev_df, double *dev_dn, double *dev_ds, 
     double *dev_de, double *dev_dw, double *dev_ym, double *dev_x, double *dev_y, double *dev_aw, double *dev_as, double *dev_ae, double *dev_an, 
     double *dev_ap, double *dev_u_w, double *dev_u_e, double *dev_u_s, double *dev_u_n, double *dev_v_p, double *dev_u_p, double *dev_dudxdx, 
-    double *dev_dxdvdy, double *dev_q_art, double *dev_xm, double *dev_liga_poros, double re, double **p, double **ru, int jmax, 
-    int i, double b_art, double darcy_number, double cf, double g){
+    double *dev_dxdvdy, double *dev_q_art, double *dev_xm, double *dev_liga_poros, double re, double **p, double **ru, int imax, 
+    int j, double b_art, double darcy_number, double cf, double g){
+    
+    int i = blockIdx.x * blockDim.x + threadIdx.x + 2;    
 
+    if(i <= imax){
         //compute x-direction velocity component un
         dev_fn[idx] = 0.5 * (vm[i][j+1]+vm[i-1][j+1]) * dev_areau_n[i] / dev_epsilon1[idx];
         dev_fs[idx] = 0.5 * (vm[i][j]+vm[i-1][j]) * dev_areau_s[i] / dev_epsilon1[idx];
@@ -57,5 +60,13 @@ __global__ void calc_upwind_ui(double *dev_fn, double *dev_fs, double *dev_fe, d
 
 //--- upwind_U ---
 void upwind_Ui(double **um, double **vm, double **p, double **ru, int j){
+    int threads = 256;
+    dim3 blocks = grid_1d(imax, threads);
 
+    calc_upwind_Ui<<<blocks, threads>>>(dev_fn, dev_fs, dev_fe, dev_fw, vm, um, dev_areau_n, dev_areau_s,
+         dev_areau_e, dev_areau_w, dev_epsilon1, dev_df, dev_dn, dev_ds, dev_de, dev_dw, dev_ym, dev_x, dev_y, dev_aw,
+         dev_as, dev_ae, dev_an, dev_ap, dev_u_w, dev_u_e, dev_u_s, dev_u_n, dev_v_p, dev_u_p, dev_dudxdx, dev_dxdvdy,
+         dev_q_art, dev_xm, dev_liga_poros, re, p, ru, imax, j, iterations.b_art, darcy_number, cf, g);
+
+    cudaDeviceSynchronize();
 }
