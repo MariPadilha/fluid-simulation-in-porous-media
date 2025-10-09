8041810fd8ae4a181d4e14078145129a4cb54496
MariPadilha <lunaforever53@gmail.com>
2025-10-09 17:41:55 -0300

    .

--- Roberta13.11.2024_copy/comp_mean.c
+++ Roberta13.11.2024_copy/comp_mean.c
@@ -7,6 +7,7 @@ void comp_mean(double **u, double **v, double **um, double **vm){
     for(i = 1; i <= imax; i++){
         for(j = 1; j <= jmax; j++){
             u[i][j] = (um[i+1][j]+um[i][j])*0.50;
+            printf("[%i][%i] = %lf\n", i, j, um[i][j]);
         }
     }
 
--- Roberta13.11.2024_copy/main.c
+++ Roberta13.11.2024_copy/main.c
@@ -280,11 +280,6 @@ int main(){
     */
 
     //--- Compute the velocity of mean points ---
-    for(i = 1; i <= imax; i++){
-        for(j = 1; j <= jmax; j++){
-            printf("[%i][%i] = %lf\n", i, j, u[i][j]);
-        }
-    }
     comp_mean(u, v, um, vm);
     transient(u, v, p, t, c, itc);
 
