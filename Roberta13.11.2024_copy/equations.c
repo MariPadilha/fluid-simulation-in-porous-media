6ba1d45c7c3e53c29d394726780a994ff3aff6e3
MariPadilha <lunaforever53@gmail.com>
2025-10-10 16:31:50 -0300

    .

--- Roberta13.11.2024_copy/equations.c
+++ Roberta13.11.2024_copy/equations.c
@@ -425,7 +425,7 @@ void RESV(double **um, double **vm, double **p, double **t, double **rv){
 
     for(int i = 1; i <= imax+1; i++){
         for(int j = 1; j <= jmax; j++){
-            printf("[%i][%i] aw = %lf\n", i, j, aw[i][j]);
+            printf("[%i][%i] ae = %lf\n", i, j, ae[i][j]);
         }
     }
 
