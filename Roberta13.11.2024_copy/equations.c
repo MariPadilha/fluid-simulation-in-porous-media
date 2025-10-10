ac31fe480f8630641dc6a28a6feb5cfb972b3ba0
MariPadilha <lunaforever53@gmail.com>
2025-10-10 16:06:21 -0300

    .

--- Roberta13.11.2024_copy/equations.c
+++ Roberta13.11.2024_copy/equations.c
@@ -333,62 +333,62 @@ void RESV(double **um, double **vm, double **p, double **t, double **rv){
             fw[i][j] = 0.5 * (um[i][j]+um[i][j-1]) * areav_w[j] / epsilon1[i][j];
 
             df[i][j] = fe[i][j] - fw[i][j] + fn[i][j] - fs[i][j];
-
+            
             dn[i][j] = (epsilon1[i][j]/re) * areav_n[i] / (ym[j+1]-ym[j]);
             ds[i][j] = (epsilon1[i][j]/re) * areav_s[i] / (ym[j]-ym[j-1]);
             de[i][j] = (epsilon1[i][j]/re) * areav_e[j] / (x[i+1]-x[i]);
             dw[i][j] = (epsilon1[i][j]/re) * areav_w[j] / (x[i]-x[i-1]);
-
+            
             //quick
             if(fw[i][j] > 0.0){
                 afw[i][j] = 1.0;
             }else if(fw[i][j] < 0.0){
                 afw[i][j] = 0.0;
             }
-
+            
             if(fe[i][j] > 0.0){
                 afe[i][j] = 1.0;
             }else if(fe[i][j] < 0.0){
                 afe[i][j] = 0.0;
             }
-
+            
             if(fn[i][j] > 0.0){
                 afn[i][j] = 1.0;
             }else if(fn[i][j] < 0.0){
                 afn[i][j] = 0.0;
             }
-
+            
             if(fs[i][j] > 0.0){
                 afs[i][j] = 1.0;
             }else if(fs[i][j] < 0.0){
                 afs[i][j] = 0.0;
             }
-
+            
             aw[i][j] = dw[i][j] + 0.75 * afw[i][j] * fw[i][j]
-                    +  0.125 * afe[i][j] * fe[i][j]
-                    +  0.375 * (1.0-afw[i][j]) * fw[i][j];
-
+            +  0.125 * afe[i][j] * fe[i][j]
+            +  0.375 * (1.0-afw[i][j]) * fw[i][j];
+            
             ae[i][j] = de[i][j] - 0.375 * afe[i][j] * fe[i][j]
-                    - 0.75 * (1.0-afe[i][j]) * fe[i][j]
-                    - 0.125 * (1.0-afw[i][j]) * fw[i][j];
-
+            - 0.75 * (1.0-afe[i][j]) * fe[i][j]
+            - 0.125 * (1.0-afw[i][j]) * fw[i][j];
+            
             as[i][j] = ds[i][j] + 0.75  * afs[i][j] * fs[i][j] 
-                    +  0.125 * afn[i][j] * fn[i][j] 
-                    +  0.375 * (1.0-afs[i][j]) * fs[i][j];
-
+            +  0.125 * afn[i][j] * fn[i][j] 
+            +  0.375 * (1.0-afs[i][j]) * fs[i][j];
+            
             an[i][j] = dn[i][j] - 0.375 * afn[i][j] * fn[i][j]
-                    - 0.75 * (1.0-afn[i][j]) * fn[i][j]
-                    - 0.125 * (1.0-afs[i][j]) * fs[i][j];
-
+            - 0.75 * (1.0-afn[i][j]) * fn[i][j]
+            - 0.125 * (1.0-afs[i][j]) * fs[i][j];
+            
             aww[i][j] = -0.125 * afw[i][j] * fw[i][j];
             aee[i][j] =  0.125 * (1.0-afe[i][j]) * fe[i][j];
             ass[i][j] = -0.125 * afs[i][j] * fs[i][j];
             ann[i][j] =  0.125 * (1.0-afn[i][j]) * fn[i][j];
-
+            
             ap[i][j] = aw[i][j] + ae[i][j] + as[i][j] + an[i][j] 
-                    +  aww[i][j] + aee[i][j] + ass[i][j] + ann[i][j] + df[i][j];
+            +  aww[i][j] + aee[i][j] + ass[i][j] + ann[i][j] + df[i][j];
             //end Quick///////////////////////////////////////////////
-
+            
             v_w[i][j]  = vm[i-1][j];
             v_ww[i][j] = vm[i-2][j];
             v_e[i][j]  = vm[i+1][j];
@@ -399,27 +399,33 @@ void RESV(double **um, double **vm, double **p, double **t, double **rv){
             v_nn[i][j] = vm[i][j+2];         
             v_p[i][j]  = vm[i][j];
             u_p[i][j]  = um[i][j];
-
+            
             dvdydy[i][j] = areav_n[i] * (v_n[i][j]-v_p[i][j]) / (ym[j+1]-ym[j])
-                        -  areav_s[i] * (v_p[i][j]-v_s[i][j]) / (ym[j]-ym[j-1]);
-
+            -  areav_s[i] * (v_p[i][j]-v_s[i][j]) / (ym[j]-ym[j-1]);
+            
             dydudx[i][j] = areav_n[i] * (um[i+1][j]-um[i][j]) / (xm[i+1]-xm[i])
-                        -  areav_s[i] * (um[i+1][j-1]-um[i][j-1]) / (xm[i+1]-xm[i]);
-
+            -  areav_s[i] * (um[i+1][j-1]-um[i][j-1]) / (xm[i+1]-xm[i]);
+            
             artdivv[i][j] = -(iterations.b_art) * (dydudx[i][j]+dvdydy[i][j]);            
-
+            
             //bulk artificial viscosity term from Ramshaw(1990)
             q_art[i][j] = epsilon1[i][j] * (p[i][j]-p[i][j-1]) / (y[j]-y[j-1]) + artdivv[i][j];
-
+            
             rv[i][j] = 1.0 / (x[i]-x[i-1]) / (y[j]-y[j-1]) 
-                    * (-ap[i][j] * v_p[i][j] + aww[i][j] * v_ww[i][j] + aw[i][j] 
-                    * v_w[i][j] + aee[i][j] * v_ee[i][j] + ae[i][j] * v_e[i][j] 
-                    + ass[i][j] * v_ss[i][j] + as[i][j] * v_s[i][j] 
-                    + ann[i][j] * v_nn[i][j] + an[i][j] * v_n[i][j]) 
-                    - q_art[i][j] + invfr2 * (1.0 - 1.0 / ((t[i][j]+t[i][j-1]) * 0.5))
-                    - epsilon1[i][j]*(v_p[i][j]/(re*darcy_number) 
-                    + cf/(pow((epsilon1[i][j]*darcy_number), 0.5)) * v_p[i][j] 
-                    * (pow((pow(u_p[i][j], 2.0) + pow(v_p[i][j], 2.0)), 0.5))) * liga_poros[i][j]; 
+            * (-ap[i][j] * v_p[i][j] + aww[i][j] * v_ww[i][j] + aw[i][j] 
+                * v_w[i][j] + aee[i][j] * v_ee[i][j] + ae[i][j] * v_e[i][j] 
+                + ass[i][j] * v_ss[i][j] + as[i][j] * v_s[i][j] 
+                + ann[i][j] * v_nn[i][j] + an[i][j] * v_n[i][j]) 
+                - q_art[i][j] + invfr2 * (1.0 - 1.0 / ((t[i][j]+t[i][j-1]) * 0.5))
+                - epsilon1[i][j]*(v_p[i][j]/(re*darcy_number) 
+                + cf/(pow((epsilon1[i][j]*darcy_number), 0.5)) * v_p[i][j] 
+                * (pow((pow(u_p[i][j], 2.0) + pow(v_p[i][j], 2.0)), 0.5))) * liga_poros[i][j]; 
+        }
+    }
+
+    for(int i = 1; i <= imax; i++){
+        for(int j = 1; j <= jmax+1; j++){
+            printf("[%i][%i] rv = %lf\n", i, j, rv[i][j]);
         }
     }
 
@@ -561,12 +567,6 @@ void solve_V(double **um, double **vm, double **vm_n, double **um_tau, double **
         res_v[i] = (double*)malloc(sizeof(double)*(jmax+2));
     }
     
-    /*for(i = 1; i <= imax; i++){
-        for(j = 1; j <= jmax; j++){                
-            printf("vm_n_tau [%d][%d] = %.14lf\n", i, j, vm_n_tau[i][j]);
-            }
-            }*/
-           
     RESV(um_tau,vm_tau,p,T,RV);
     
     for(j = 3; j <= jmax-1; j++){
@@ -576,11 +576,6 @@ void solve_V(double **um, double **vm, double **vm_n, double **um_tau, double **
         }
     }
     
-    for(int i = 1; i <= imax; i++){
-        for(int j = 1; j <= jmax+1; j++){
-            printf("[%i][%i] res_v = %lf\n", i, j, res_v[i][j]);
-        }
-    }
 
     bcUV(um_tau,vi);
     RESV(um_tau,vi,p,T,RV);
